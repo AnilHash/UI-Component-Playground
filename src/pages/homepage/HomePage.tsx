29354bae5db0df96aa749830e28e0f50490e56eb
AnilHash <anilaraganji123@gmail.com>
2025-11-24 13:29:32 +0530

    adding homepage with styles

---
+++ src/pages/homepage/HomePage.tsx
@@ -0,0 +1,27 @@
+import "./homepage.style.css";
+function HomePage() {
+  return (
+    <article className="container">
+      <h2>
+        UI Component{" "}
+        <p className="wrapped">
+          <span>P</span>
+          <span>l</span>
+          <span>a</span>
+          <span>y</span>
+          <span>g</span>
+          <span>r</span>
+          <span>o</span>
+          <span>u</span>
+          <span>n</span>
+          <span>d</span>
+        </p>
+      </h2>
+      <h4>
+        Explore and customize a set of reusable components bullt with React.
+      </h4>
+    </article>
+  );
+}
+
+export default HomePage;
